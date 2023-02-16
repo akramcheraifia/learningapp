592b011a59e75d05291e96326be0d33554a3b2a3
akramcheraifia <hebdu636dheb@gmail.com>
2023-02-16 21:53:38 +0100

    code improvments

--- lib/modules/login/login_screen.dart
+++ lib/modules/login/login_screen.dart
@@ -8,7 +8,7 @@ class LoginScreen extends StatelessWidget {
   final emailController = TextEditingController();
   @required
   final passwordController = TextEditingController();
-
+  var formKey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,64 +16,82 @@ class LoginScreen extends StatelessWidget {
         padding: const EdgeInsets.all(20),
         child: Center(
           child: SingleChildScrollView(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Container(
-                  alignment: AlignmentDirectional.topStart,
-                  child: const Text(
-                    'Login',
-                    style: TextStyle(fontSize: 50, fontWeight: FontWeight.bold),
-                  ),
-                ),
-                const SizedBox(height: 40),
-                TextFormField(
-                  controller: emailController,
-                  keyboardType: TextInputType.emailAddress,
-                  onChanged: (value) => print(value),
-                  onSaved: (newValue) => print(newValue),
-                  decoration: const InputDecoration(
-                    labelText: 'Enter your email',
-                    border: OutlineInputBorder(),
-                    prefixIcon: Icon(Icons.email),
-                  ),
-                ),
-                const SizedBox(height: 20),
-                TextFormField(
-                  controller: passwordController,
-                  keyboardType: TextInputType.visiblePassword,
-                  obscureText: true,
-                  onChanged: (value) {},
-                  onSaved: (value) {},
-                  decoration: const InputDecoration(
-                      labelText: 'Enter your password',
-                      border: OutlineInputBorder(),
-                      prefixIcon: Icon(Icons.lock),
-                      suffixIcon: Icon(Icons.remove_red_eye)),
-                ),
-                const SizedBox(height: 20),
-                defaultButton(
-                  width: 200,
-                  height: 50,
-                  back: Colors.red,
-                  onPressed: () {
-                    print(emailController.text);
-                    print(passwordController.text);
-                  },
-                  text: 'Login',
-                ),
-                const SizedBox(height: 20),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    const Text('Don\'t have an account?'),
-                    TextButton(
-                      onPressed: () {},
-                      child: const Text('Register Now'),
+            child: Form(
+              key: formKey,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Container(
+                    alignment: AlignmentDirectional.topStart,
+                    child: const Text(
+                      'Login',
+                      style:
+                          TextStyle(fontSize: 50, fontWeight: FontWeight.bold),
                     ),
-                  ],
-                ),
-              ],
+                  ),
+                  const SizedBox(height: 40),
+                  defaultTextformfield(
+                      Controller: emailController,
+                      label: 'Enter your email',
+                      onSubmit: (value) {
+                        print(value);
+                      },
+                      onChange: (value) {
+                        print(value);
+                      },
+                      type: TextInputType.emailAddress,
+                      validator: (value) {
+                        if (value!.isEmpty) {
+                          return 'Please enter your email';
+                        }
+                        return null;
+                      },
+                      isPassword: false,
+                      prefix: Icons.email),
+                  const SizedBox(height: 20),
+                  defaultTextformfield(
+                      Controller: passwordController,
+                      label: 'enter your password',
+                      onSubmit: (value) {
+                        print(value);
+                      },
+                      onChange: (value) {
+                        print(value);
+                      },
+                      type: TextInputType.visiblePassword,
+                      validator: (value) {
+                        if (value!.isEmpty) {
+                          return 'Please enter your password';
+                        }
+                      },
+                      prefix: Icons.lock,
+                      suffix: Icons.remove_red_eye,
+                      isPassword: true),
+                  const SizedBox(height: 20),
+                  defaultButton(
+                    height: 50,
+                    back: Colors.red,
+                    onPressed: () {
+                      if (formKey.currentState!.validate()) {
+                        print(emailController.text);
+                        print(passwordController.text);
+                      }
+                    },
+                    text: 'Login',
+                  ),
+                  const SizedBox(height: 20),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const Text('Don\'t have an account?'),
+                      TextButton(
+                        onPressed: () {},
+                        child: const Text('Register Now'),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
             ),
           ),
         ),
--- lib/shared/components/components.dart
+++ lib/shared/components/components.dart
@@ -22,3 +22,30 @@ Widget defaultButton({
     ),
   );
 }
+
+Widget defaultTextformfield({
+  // ignore: non_constant_identifier_names
+  required TextEditingController Controller,
+  required String label,
+  required final void Function(String?) onSubmit,
+  required final void Function(String?) onChange,
+  required TextInputType type,
+  required final String? Function(String?) validator,
+  bool isPassword = false,
+  required IconData prefix,
+  IconData? suffix,
+}) {
+  return TextFormField(
+    validator: validator,
+    controller: Controller,
+    keyboardType: type,
+    onFieldSubmitted: onSubmit,
+    onChanged: onChange,
+    decoration: InputDecoration(
+      labelText: label,
+      border: const OutlineInputBorder(),
+      prefixIcon: Icon(prefix),
+      suffixIcon: suffix != null ? Icon(suffix) : null,
+    ),
+  );
+}
